083e3e8705f7c581b2528511e9b6a13d4864aacd
unovongalixor <smnbursten@gmail.com>
2025-11-12 21:41:34 +0200

    try deleting utest dir

--- openblas-build/src/download.rs
+++ openblas-build/src/download.rs
@@ -27,7 +27,7 @@ pub fn download(out_dir: &Path) -> Result<PathBuf> {
         ar.unpack(out_dir)?;
         assert!(dest.exists());
 
-        std::fs::remove_dir(dest.join("utest")).unwrap();
+        std::fs::remove_dir_all(dest.join("utest")).unwrap();
     }
     Ok(dest)
 }
